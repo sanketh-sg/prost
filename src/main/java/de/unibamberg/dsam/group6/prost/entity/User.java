fd6c27ae1fe85dc5ee484e428943cb21526e02d6
Sanketh S G <sankethsg2944@gmail.com>
2026-08-31 00:04:25 +0200

    refactor: improve formatting of getAuthorities method for better readability

--- src/main/java/de/unibamberg/dsam/group6/prost/entity/User.java
+++ src/main/java/de/unibamberg/dsam/group6/prost/entity/User.java
@@ -68,7 +68,9 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).toList();
+        return this.roles.stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .toList();
     }
 
     @Override
